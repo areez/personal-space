c7b0b98fdce83bbb5d3e0d8ae47495f299e65a3a
areez <areez.bd@gmail.com>
2025-09-11 21:27:22 +0600

    31st commit Blogs added wo images

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -52,7 +52,7 @@ const home: Home = {
     display: true,
     title: (
       <Row gap="12" vertical="center">
-        <strong className="ml-4">Areez's</strong>{" "}
+        <strong className="ml-4">Areez&apos;s</strong>{" "}
         <Line background="brand-alpha-strong" vert height="20" />
         <Text marginRight="4" onBackground="brand-medium">
           Featured Thought
